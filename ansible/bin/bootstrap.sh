372e0a62c4dd7ad07bb7476e8ee45456ec9e8ec4
Ryan White <ryanmackenziewhitephd@gmail.com>
2018-12-30 13:01:05 -0500

    create ansible playbook

---
+++ ansible/bin/bootstrap.sh
@@ -0,0 +1,11 @@
+#! /bin/sh
+#
+# bootstrap.sh
+# Copyright (C) 2018 Ryan Mackenzie White <ryan.white4@canada.ca>
+#
+# Distributed under terms of the  license.
+#
+
+echo "Installing ansible for provisioning for \"${USER}\" "
+
+sudo yum -y install ansible
